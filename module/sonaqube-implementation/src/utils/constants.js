48992666d668579c22ac966917ae7a9d5c9ee3a2
Basanta Maharjan <basantamaharjan@lftechnology.com>
2024-12-18 15:14:02 +0545

    Integrate

--- module/sonaqube-implementation/src/utils/constants.js
+++ module/sonaqube-implementation/src/utils/constants.js
@@ -1,19 +1,21 @@
-const repoOwner = 'shresthasamir4119';
-const repoName = 'agent-bass-test';
-const baseBranch = 'main';
-const newBranch = baseBranch+'agent-bass-branch';
-const prTitle = newBranch + ' Bot-generated Fix for Sonar Code Quality Issue';
-const prBody = 'This pull request is bot-generated and automatically addresses and resolves the selected issue, implementing a fix to ensure proper functionality.';
-const token = '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'; //agent-bass-bot token
-const commitMessage = newBranch+ ' Bot-generated fix for Sonar code quality issue';
+const repoOwner = "Basanta78";
+const repoName = "agent-bass";
+const baseBranch = "main";
+const newBranch = "agent-bass-branch";
+const prTitle = newBranch + " Bot-generated Fix for Sonar Code Quality Issue";
+const prBody =
+  "This pull request is bot-generated and automatically addresses and resolves the selected issue, implementing a fix to ensure proper functionality.";
+const token = "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"; //agent-bass-bot token
+const commitMessage =
+  newBranch + " Bot-generated fix for Sonar code quality issue";
 
 export default {
-    repoOwner,
-    repoName,
-    baseBranch,
-    newBranch,
-    prTitle,
-    prBody,
-    token,
-    commitMessage
-}
+  repoOwner,
+  repoName,
+  baseBranch,
+  newBranch,
+  prTitle,
+  prBody,
+  token,
+  commitMessage,
+};
