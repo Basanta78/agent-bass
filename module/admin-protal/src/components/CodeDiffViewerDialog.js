159676e0b26c0130a008803af39050906fc0fcdb
Samir Shrestha <57619458+shresthasamir4119@users.noreply.github.com>
2024-12-19 12:42:32 +0545

    Fix CodeDiffViewerDialog

--- module/admin-protal/src/components/CodeDiffViewerDialog.js
+++ module/admin-protal/src/components/CodeDiffViewerDialog.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { Diff } from 'diff';
-import Diff2Html from 'diff2html';
+import * as Diff from 'diff';
+import { html } from 'diff2html';
 import 'diff2html/bundles/css/diff2html.min.css';
 
 const CodeDiffViewer = ({ code1, code2 }) => {
@@ -8,30 +8,22 @@ const CodeDiffViewer = ({ code1, code2 }) => {
 
   useEffect(() => {
     // Compute the diff between the two code versions
-    const diff = Diff.createTwoFilesPatch('Version 1', 'Version 2', code1, code2);
+    const diff = Diff.createTwoFilesPatch('File', 'File', code1, code2);
 
     // Convert the diff to HTML using diff2html
-    const html = Diff2Html.html(diff, {
-      drawFileList: false, // Hide file list
+    const diffHtml = html(diff, {
+      drawFileList: true, // Hide file list
       matching: 'words',   // Word matching for more accurate diffs
       outputFormat: 'side-by-side', // Side-by-side format
     });
 
     // Set the HTML diff content
-    setDiffHtml(html);
+    setDiffHtml(diffHtml);
   }, [code1, code2]);
 
   return (
     <div>
       <h1>Code Diff Viewer</h1>
-      <div className="code" style={codeStyle}>
-        <h3>Code Version 1:</h3>
-        <pre>{code1}</pre>
-      </div>
-      <div className="code" style={codeStyle}>
-        <h3>Code Version 2:</h3>
-        <pre>{code2}</pre>
-      </div>
       <div id="diff-container" style={diffContainerStyle}>
         <h3>Code Diff:</h3>
         <div id="diff-output" dangerouslySetInnerHTML={{ __html: diffHtml }} />
